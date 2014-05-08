e490dacfe4133de7cb1f4d92aa1929c7286b9e9f
Matt.Hill <lepermessiah57@gmail.com>
2014-05-08 09:33:28 -0500

    change counting finished

--- src/Kata/ChangeMachine.php
+++ src/Kata/ChangeMachine.php
@@ -5,7 +5,32 @@ namespace Kata;
 class ChangeMachine {
 
     public function makeChange($money){
+        $cash = $money * 100;
 
+        list($quarter, $cash) = $this->countChange($cash, 25);
+        list($dime, $cash) = $this->countChange($cash, 10);
+        list($nickel, $cash) = $this->countChange($cash, 5);
+        list($penny, $cash) = $this->countChange($cash, 1);
+
+        $values = ['penny'=>$penny, 'nickel'=>$nickel, 'dime'=>$dime, 'quarter'=>$quarter];
+        $retval = [];
+        foreach($values as $key => $value){
+            if($value > 0){
+                $retval[$key] = $value;
+            }
+        }
+
+        return $retval;
     }
+
+    private function countChange($cash, $value) {
+        $change = 0;
+        while ($cash - $value >= 0) {
+            $change++;
+            $cash -= $value;
+        }
+        return array($change, $cash);
+    }
+
 
 } 
--- test/Kata/ChangeMachineTest.php
+++ test/Kata/ChangeMachineTest.php
@@ -20,5 +20,94 @@ class ChangeMachineTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals(['penny'=>1], $change);
     }
 
+    public function testMakeChangeTwoCentsReturnsTwoPenny(){
+        $money = .02;
+        $changeMachine = new ChangeMachine();
+
+        $change = $changeMachine->makeChange($money);
+
+        $this->assertEquals(['penny'=>2], $change);
+    }
+
+    public function testMakeChangeFiveCentsReturnsOneNickel(){
+        $money = .05;
+        $changeMachine = new ChangeMachine();
+
+        $change = $changeMachine->makeChange($money);
+
+        $this->assertEquals(['nickel'=>1], $change);
+    }
+
+    public function testMakeChangeSixCentsReturnsOneNickelAndOnePenny(){
+        $money = .06;
+        $changeMachine = new ChangeMachine();
+
+        $change = $changeMachine->makeChange($money);
+
+        $this->assertEquals(['nickel'=>1, 'penny'=>1], $change);
+    }
+
+    public function testMakeChangeTenCentsReturnsOneDime(){
+        $money = .10;
+        $changeMachine = new ChangeMachine();
+
+        $change = $changeMachine->makeChange($money);
+
+        $this->assertEquals(['dime'=>1], $change);
+    }
+
+    public function testMakeChangeTwelveCentsReturnsOneDimeAndTwoPennies(){
+        $money = .12;
+        $changeMachine = new ChangeMachine();
+
+        $change = $changeMachine->makeChange($money);
+
+        $this->assertEquals(['dime'=>1, 'penny'=>2], $change);
+    }
+
+    public function testMakeChangeFifteenCentsReturnsOneDimeAndOneNickel(){
+        $money = .15;
+        $changeMachine = new ChangeMachine();
+
+        $change = $changeMachine->makeChange($money);
+
+        $this->assertEquals(['dime'=>1, 'nickel'=>1], $change);
+    }
+
+    public function testMakeChangeSixteenCentsReturnsOneDimeAndOneNickelAndOnePenny(){
+        $money = .16;
+        $changeMachine = new ChangeMachine();
+
+        $change = $changeMachine->makeChange($money);
+
+        $this->assertEquals(['dime'=>1, 'nickel'=>1, 'penny'=>1], $change);
+    }
+
+    public function testMakeChangeTwentyCentsReturnsTwoDimes(){
+        $money = .20;
+        $changeMachine = new ChangeMachine();
+
+        $change = $changeMachine->makeChange($money);
+
+        $this->assertEquals(['dime'=>2], $change);
+    }
+
+    public function testMakeChangeTwentyFiveCentsReturnsOneQuarter(){
+        $money = .25;
+        $changeMachine = new ChangeMachine();
+
+        $change = $changeMachine->makeChange($money);
+
+        $this->assertEquals(['quarter'=>1], $change);
+    }
+
+    public function testMakeChangeFortyTwoCentsReturnsOneQuarterOneDimeOneNickelAndTwoPennies(){
+        $money = .42;
+        $changeMachine = new ChangeMachine();
+
+        $change = $changeMachine->makeChange($money);
+
+        $this->assertEquals(['quarter'=>1, 'dime'=>1, 'nickel'=>1,'penny'=>2], $change);
+    }
 }
  
