c42a6caa2cc745ed5f87aeb659319b3656a4055a
Matt.Hill <lepermessiah57@gmail.com>
2014-05-08 11:07:21 -0500

    minor refactoring

--- src/Kata/ChangeMachine.php
+++ src/Kata/ChangeMachine.php
@@ -7,10 +7,10 @@ class ChangeMachine {
     public function makeChange($money){
         $cash = $money * 100;
 
-        list($quarter, $cash) = $this->countChange($cash, 25);
-        list($dime, $cash) = $this->countChange($cash, 10);
-        list($nickel, $cash) = $this->countChange($cash, 5);
-        list($penny, $cash) = $this->countChange($cash, 1);
+        $quarter = $this->countChange($cash, 25);
+        $dime = $this->countChange($cash, 10);
+        $nickel = $this->countChange($cash, 5);
+        $penny = $this->countChange($cash, 1);
 
         $values = ['penny'=>$penny, 'nickel'=>$nickel, 'dime'=>$dime, 'quarter'=>$quarter];
         $retval = [];
@@ -23,13 +23,13 @@ class ChangeMachine {
         return $retval;
     }
 
-    private function countChange($cash, $value) {
+    private function countChange(&$cash, $value) {
         $change = 0;
         while ($cash - $value >= 0) {
             $change++;
             $cash -= $value;
         }
-        return array($change, $cash);
+        return $change;
     }
 
 
