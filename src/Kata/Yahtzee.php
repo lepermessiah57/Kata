cd442d679eeb11098a8972998c628438fed38201
Matt.Hill <lepermessiah57@gmail.com>
2014-05-14 15:39:13 -0500

    refactoring yahtzee

--- src/Kata/Yahtzee.php
+++ src/Kata/Yahtzee.php
@@ -6,35 +6,19 @@ namespace Kata;
 
 use Kata\ScoringRules\ChanceRule;
 use Kata\ScoringRules\YahtzeeRule;
+use Kata\ScoringRules\PairRule;
 
 class Yahtzee {
 
     public function getPossibleScores($dice){
-        $chance = new ChanceRule();
-        $chance_score = $chance->score($dice);
+        $scores = [];
+        $rules = [new ChanceRule(), new YahtzeeRule(), new PairRule()];
 
-        $yahtzee = new YahtzeeRule();
-        $yahtzee_score = $yahtzee->score($dice);
-
-
-        $possible_dice = [1=>0, 2=>0, 3=>0, 4=>0, 5=>0, 6=>0];
-        foreach($dice as $die){
-            $possible_dice[$die]++;
-        }
-        $pair_score = 0;
-        foreach($possible_dice as $value => $count){
-            if( $count >= 2 ) {
-                $pair_score = $value * 2;
-            }
+        foreach($rules as $rule){
+            $scores[$rule->getScoreName()] = $rule->score($dice);
         }
 
-        $possible_scores = array(
-            $chance->getScoreName()  => $chance_score,
-            $yahtzee->getScoreName() => $yahtzee_score,
-            'pair'    => $pair_score
-        );
-
-        return $possible_scores;
+        return $scores;
     }
 
 } 
