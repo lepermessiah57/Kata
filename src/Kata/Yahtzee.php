d1fc6c84c5455194b3b04c71788f51559de9ee18
Matt.Hill <lepermessiah57@gmail.com>
2014-05-14 15:04:45 -0500

    implemented pair

--- src/Kata/Yahtzee.php
+++ src/Kata/Yahtzee.php
@@ -10,10 +10,21 @@ class Yahtzee {
         $chance_score = array_sum($dice);
         $yahtzee = count(array_unique($dice)) == 1;
         $yahtzee_score = $yahtzee ? 50 : 0;
+        $possible_dice = [1=>0, 2=>0, 3=>0, 4=>0, 5=>0, 6=>0];
+        foreach($dice as $die){
+            $possible_dice[$die]++;
+        }
+        $pair_score = 0;
+        foreach($possible_dice as $value => $count){
+            if( $count >= 2 ) {
+                $pair_score = $value * 2;
+            }
+        }
 
         $possible_scores = array(
             'chance'  => $chance_score,
-            'yahtzee' => $yahtzee_score
+            'yahtzee' => $yahtzee_score,
+            'pair'    => $pair_score
         );
 
         return $possible_scores;
--- test/Kata/YahtzeeTest.php
+++ test/Kata/YahtzeeTest.php
@@ -19,6 +19,23 @@ class YahtzeeTest extends \PHPUnit_Framework_TestCase {
         $this->assertCategoryExists('chance', $actual);
     }
 
+    public function testPossibleScoresReturnsUsYahtzee(){
+        $dice = [1,1,1,1,1];
+
+        $actual = $this->game->getPossibleScores($dice);
+
+        $this->assertCategoryExists('yahtzee', $actual);
+    }
+
+    public function testPossibleScoresReturnsUsPair(){
+        $dice = [1,1,1,1,1];
+
+        $actual = $this->game->getPossibleScores($dice);
+
+        $this->assertCategoryExists('pair', $actual);
+    }
+
+
     public function testPossibleScoresChanceAllOnesReturnsFive(){
         $dice = [1,1,1,1,1];
         $expected = 5;
@@ -49,14 +66,6 @@ class YahtzeeTest extends \PHPUnit_Framework_TestCase {
         $this->assertCategoryScore($expected, $category, $actual);
     }
 
-    public function testPossibleScoresReturnsUsYahtzee(){
-        $dice = [1,1,1,1,1];
-
-        $actual = $this->game->getPossibleScores($dice);
-
-        $this->assertCategoryExists('yahtzee', $actual);
-    }
-
     public function testPossibleScoresYahtzeeAllOnesReturnsFifty(){
         $dice = [1,1,1,1,1];
         $category = 'yahtzee';
@@ -80,6 +89,66 @@ class YahtzeeTest extends \PHPUnit_Framework_TestCase {
     public function testPossibleScoresYahtzeeRandomDigitsReturnsZero(){
         $dice = [1,2,5,1,4];
         $category = 'yahtzee';
+        $expected = 0;
+
+        $actual = $this->game->getPossibleScores($dice);
+
+        $this->assertCategoryScore($expected, $category, $actual);
+    }
+
+    public function testPossibleScoresPairSinglePairOfOnesReturnsUsTwo(){
+        $dice = [1,2,5,1,4];
+        $category = 'pair';
+        $expected = 2;
+
+        $actual = $this->game->getPossibleScores($dice);
+
+        $this->assertCategoryScore($expected, $category, $actual);
+    }
+
+    public function testPossibleScoresPairSinglePairOfTwosReturnsUsFour(){
+        $dice = [1,2,5,2,4];
+        $category = 'pair';
+        $expected = 4;
+
+        $actual = $this->game->getPossibleScores($dice);
+
+        $this->assertCategoryScore($expected, $category, $actual);
+    }
+
+    public function testPossibleScoresPairSinglePairOfThreesReturnsUsSix(){
+        $dice = [1,2,5,3,3];
+        $category = 'pair';
+        $expected = 6;
+
+        $actual = $this->game->getPossibleScores($dice);
+
+        $this->assertCategoryScore($expected, $category, $actual);
+    }
+
+    public function testPossibleScoresPairAllThreesReturnsUsSix(){
+        $dice = [3,3,3,3,3];
+        $category = 'pair';
+        $expected = 6;
+
+        $actual = $this->game->getPossibleScores($dice);
+
+        $this->assertCategoryScore($expected, $category, $actual);
+    }
+
+    public function testPossibleScoresPairTwoThreesTwoFivesAndOneOneReturnsUsTen(){
+        $dice = [5,3,5,1,3];
+        $category = 'pair';
+        $expected = 10;
+
+        $actual = $this->game->getPossibleScores($dice);
+
+        $this->assertCategoryScore($expected, $category, $actual);
+    }
+
+    public function testPossibleScoresPairNoPairReturnsUsZero(){
+        $dice = [5,3,4,1,2];
+        $category = 'pair';
         $expected = 0;
 
         $actual = $this->game->getPossibleScores($dice);
